023cb6f4c96f0e2625e3e32aa1bc90f77e93afa5
prondel2 <102826086+prondel2@users.noreply.github.com>
2024-06-04 17:45:48 +0200

    Update main.tf-9

--- reverse_shell/main.tf
+++ reverse_shell/main.tf
@@ -9,6 +9,12 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_rds_cluster" "app2-rds-cluster" {
+  cluster_identifier      = "app2-rds-cluster"
+  allocated_storage       = 10
+  backup_retention_period = 1
+}
+
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
 
